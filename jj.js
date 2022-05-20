ab342a9184811e2b0f723ffd9ed9d6f9d8f9c90f
tingamapuro04 <82979239+tingamapuro04@users.noreply.github.com>
2022-05-20 05:01:40 +0300

    Add eslint and use input event

--- jj.js
+++ jj.js
@@ -1,10 +1,10 @@
 const hamburger = document.querySelector('.ham');
-const nav_menu = document.querySelector('.navmenu')
+const navMenu = document.querySelector('.navmenu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  nav_menu.classList.toggle('active');
-})
+  navMenu.classList.toggle('active');
+});
 
 // get the bars of the hamburger menu
 
@@ -14,49 +14,45 @@ const contactLink = document.querySelector('.contact-link');
 
 const close = () => {
   hamburger.classList.toggle('active');
-  nav_menu.classList.toggle('active');
+  navMenu.classList.toggle('active');
 };
 
 portfolio.addEventListener('click', () => {
   close();
-})
+});
 
 aboutUs.addEventListener('click', () => {
   close();
-})
+});
 
 contactLink.addEventListener('click', () => {
   close();
-})
+});
 
-
-const name1 = document.getElementById('name_1')
-const email = document.getElementById('email_1')
-
-
-
-
+const name1 = document.getElementById('name_1');
+const email = document.getElementById('email_1');
+const text1 = document.getElementById('text_1');
 
 const form = document.getElementById('form');
-form.addEventListener('submit', function (event) {
-  // event.preventDefault()
-
+form.addEventListener('input', () => {
   // getting the values of the inputs;
-  const namevalue = name1.value.trim()
-  const emailvalue = email.value.trim()
-  console.log(namevalue)
+  const namevalue = name1.value.trim();
+  const emailvalue = email.value.trim();
+  const textvalue = text1.value.trim();
 
   const details = {
     name: namevalue,
-    email: emailvalue
-  }
+    email: emailvalue,
+    text: textvalue,
+  };
 
-  localStorage.setItem('details', JSON.stringify(details))
-})
+  localStorage.setItem('details', JSON.stringify(details));
+});
 
-const details_retrieve = JSON.parse(localStorage.getItem('details'))
+const detailsRetrieve = JSON.parse(localStorage.getItem('details'));
 
-if (details_retrieve) {
-  name1.value = details_retrieve.name
-  email.value = details_retrieve.email
+if (detailsRetrieve) {
+  name1.value = detailsRetrieve.name;
+  email.value = detailsRetrieve.email;
+  text1.value = detailsRetrieve.text;
 }
