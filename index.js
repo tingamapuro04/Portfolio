6c3286c193a4beba960c7149866498dd26566f78
tingamapuro04 <alponcemobutu100@gmail.com>
2022-08-13 12:12:53 +0300

    Complete card styling

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 const projectInfo = [
   {
     name: 'Movie Maze',
-    history: ['CANOPY', 'Front End Dev', 2022],
+    history: ['Front End Dev', 'Microverse', 2022],
     fimage: './photos/Maze.jpeg',
     description: "An app that fetches data from an API and renders it to the UI",
-    technologies: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
+    technologies: ['Html', 'css', 'JavaScript', 'github', 'ruby', 'Bootstrap'],
     sLive: 'https://movie-maze.netlify.app/',
-    lSource: 'https://github.com/tingamapuro04',
   },
   {
     name: 'Book Store',
-    history: ['CANOPY', 'Facebook', 'Full Stack', 'Back End Dev', 2022],
+    history: ['Front End Dev', 'Microverse', 2022],
     fimage: './photos/buku.jpeg',
     description: "An app where we use Redux store to store data fetched from an API and also we can add and remove the items in the store",
-    technologies: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
+    technologies: ['React', "Redux", 'JavaScript'],
     sLive: 'https://beautiful-puffpuff-9f47fc.netlify.app/',
   },
   {
     name: 'Trading App',
-    history: ['CANOPY', 'Facebook', 'Full Stack', 'Back End Dev', 2022],
+    history: ['Front End Dev', 'Microverse', 2022],
     fimage: './photos/trad.jpeg',
     description: "An app where a user is able to follow the trends so that they can trade with good information about the market",
-    technologies: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
+    technologies: ['React', "Redux", 'JavaScript'],
     sLive: 'https://m-trading-app.netlify.app/',
   },
   {
     name: 'Calculator',
-    history: ['Canopy', 'Uber', 'Lead Developer', 'Back End Dev', 2015],
+    history: ['Front End Dev', 'Microverse',  2015],
     fimage: '/photos/Kalku.jpeg',
     description: "A simple calculator that can be used for simple mathematical manipulations. It is made using React",
-    technologies: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
+    technologies: ['React', 'JavaScript', 'Css'],
     sLive: 'https://neon-puffpuff-9947bf.netlify.app/',
   },
 ];
@@ -45,17 +44,17 @@ function card(item) {
     <div class="details">
       <h1>${item.name}</h1>
       <ul class="unordered">
-        <li>${item.history[0]}</li>
-        <li>${item.history[1]}</li>
+        <li class="first">${item.history[0]}</li>
+        <li class="first">${item.history[1]}</li>
         <li>${item.history[2]}</li>
       </ul>
       <p>${item.description}</p>
-      <ul class="unordered">
+      <ul class="unordered covered">
         <li>${item.technologies[0]}</li>
         <li>${item.technologies[1]}</li>
         <li>${item.technologies[2]}</li>
       </ul>
-      <a href=${item.sLive}><button>See Project</button></a>
+      <a href=${item.sLive}><button class="Project_see">See Project</button></a>
     </div>
   </div>
   `
